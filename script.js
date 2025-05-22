3b1cbfa7c84ace7cf52c1b513a836606fcaf0339
Jatin <jatinverma2428@gmail.com>
2025-05-22 16:11:57 +0530

    add record

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
- let btn = document.getElementById("btn1");
+let btn = document.getElementById("btn1");
 
-    btn.addEventListener("click", () => {
-        let original = document.getElementById("main");
-        let clone = original.cloneNode(true);
-        document.getElementById("clone-container").appendChild(clone);
+btn.addEventListener("click", () => {
+    let original = document.getElementById("main");
+    let clone = original.cloneNode(true);
+    let hulk = clone.querySelectorAll("input, select");
+    hulk.forEach(child => {
+        if (child.tagName === "SELECT") {
+            child.selectedIndex = 0;
+        } else {
+            child.value = "";
+        }
     });
+    document.getElementById("clone-container").appendChild(clone);
+});
+
+
+
+
+
